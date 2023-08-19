d73ab362e129771e69356c87be581ac980006d28
Beny Ab <140263588+habeshacorner@users.noreply.github.com>
2023-08-19 22:42:36 +0400

    video beginner and intermediate

--- script.js
+++ script.js
@@ -1,35 +1,8 @@
-const audioTable= document.querySelector("#audioTable");
+var video1=document.querySelector("#video1");
+var vc1=document.querySelector("#vc1");
+var counter1= 0 ;
 
-const audioData = [
-    {name: "Martin Luther King Jr", src:"https://www.freedomarchives.org/audio_samples/Mp3_files/MLK_Something_happening.mp3"},
-    {name: "King Haile Selassie I", src:"/Users/TJ/Documents/FullStock2023/Audio/Majesty King Selassie I.mp3"},
-    {name: "Bob Marley", src:"/Users/TJ/Documents/FullStock2023/Audio/BobMarley.mp3"},
-];
-
- for (let i = 0; i < audioData.length; i++) {
-
-    const tr = document.createElement("tr");
-
-    const td = document.createElement("td");
-    td.innerHTML = audioData[i].name;
-    tr.appendChild(td);
-    
-    const td2 = document.createElement("td");
-    const audio = document.createElement("audio");
-    audio.setAttribute("controls","");
-
-    const audioSource = document.createElement("source");
-    audioSource.setAttribute("src", audioData[i].src);
-    audio.appendChild(audioSource);
-    td2.appendChild(audio);
-    
-    tr.appendChild(td2);
-
-    
-   
-    
-
-    
-
-    audioTable.appendChild(tr);
-}
+video1.addEventListener("play", function(){
+    counter1++;
+    vc1.innerHTML = counter1;
+})
